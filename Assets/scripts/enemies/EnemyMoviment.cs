740e5c9ca819d6ec18725fd7ede54b5beb3268c4
Diegobrix <dishi.ata@gmail.com>
2023-07-28 20:36:14 -0300

    Mecânica Básica de Inimigo Seguir Player - Finalizada

--- Assets/scripts/enemies/EnemyMoviment.cs
+++ Assets/scripts/enemies/EnemyMoviment.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMoviment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent enemyNavMesh;
+    [SerializeField] private float minDistanceToPlayer; 
+    public Transform player;
+    private float rotationSpeed = 5f;
+    
+    void Awake()
     {
-        
+        enemyNavMesh = GetComponent<NavMeshAgent>();
+        enemyNavMesh.updateRotation = false;
+
+        minDistanceToPlayer = 5f;
+        enemyNavMesh.stoppingDistance = minDistanceToPlayer;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        enemyNavMesh.SetDestination(player.position);
+        SetEnemyRotation();
+    }
+
+    private void SetEnemyRotation()
+    {
+        Vector3 playerDirection = transform.position - player.position;
+        playerDirection.y = 0f;
+
+        if(playerDirection != Vector3.zero)
+        {
+            Quaternion enemyTargetRotation = Quaternion.LookRotation(playerDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, enemyTargetRotation, Time.deltaTime * rotationSpeed);
+        }
     }
 }
