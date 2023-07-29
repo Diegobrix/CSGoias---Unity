6a4bef51f5e99c9e252b0b1234dc62d5d3858100
Diegobrix <dishi.ata@gmail.com>
2023-07-29 01:20:40 -0300

    Mecânica de Pegar Arma do Chão - Finalizada

---
+++ Assets/scripts/gun/GunPickUp.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPickUp : InteractionsMessage, IInteractable
+{
+    private Transform player;
+
+    public void Awake()
+    {
+        msg = "Pegar Arma";
+        player = GameObject.Find("Player").transform;
+    }
+
+    public void Interact()
+    {
+        PickGun();
+    }
+
+    private void PickGun()
+    {
+        transform.GetComponent<Rigidbody>().isKinematic = true;
+        transform.GetComponent<BoxCollider>().enabled = false;
+
+        transform.position = player.position;
+        transform.rotation = player.rotation;
+        transform.SetParent(player);
+    }
+}
